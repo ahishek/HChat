272f3bf236d3ecd976e48298b19c93941cc73165
Abhishek Nair <ahishek@gmail.com>
2016-05-21 14:01:45 +0530

    Chat screen xml created

--- app/src/main/java/com/example/abhisheknair/hchat/MainActivity.java
+++ app/src/main/java/com/example/abhisheknair/hchat/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.abhisheknair.hchat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initControls();
+    }
+
+    private void initControls() {
+        AppCompatButton button = (AppCompatButton) findViewById(R.id.btn_login);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_login:
+
+        }
     }
 }
